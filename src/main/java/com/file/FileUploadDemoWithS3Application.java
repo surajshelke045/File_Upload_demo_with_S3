898e0431b6a19fe71cd8f9b2abdd55fe20c0a449
suraj shelke <shelkesuraj2000@gmail.com>
2023-10-19 12:59:11 +0530

    Create Project

---
+++ src/main/java/com/file/FileUploadDemoWithS3Application.java
@@ -0,0 +1,13 @@
+package com.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadDemoWithS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadDemoWithS3Application.class, args);
+	}
+
+}
